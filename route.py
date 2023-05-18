875a2e991f34b0d8a322b19ad348adf3c04fcbfe
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-05-18 23:46:40 +0530

    Update route.py

--- route.py
+++ route.py
@@ -11,3 +11,11 @@ routes = web.RouteTableDef()
 async def root_route_handler(request):
 
     return web.json_response("AnimeDrobe")
+
+app = web.AppRunner(await web_server())
+
+        await app.setup()
+
+        bind_address = "0.0.0.0"
+
+        await web.TCPSite(app, bind_address, PORT).start()
