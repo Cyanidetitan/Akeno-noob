d8c84498c9f2cd0c2086671011d4760b673c0ea6
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-05-18 23:59:51 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ async def run_app():
     app = web.AppRunner(await web_server())
     await app.setup()
     bind_address = "0.0.0.0"
-    PORT = 8080  # Replace with the desired port number
+    PORT = 8000  # Replace with the desired port number
     await web.TCPSite(app, bind_address, PORT).start()
 
 asyncio.run(run_app())
